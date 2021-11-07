6b0d84e8b18cb28f9ebc4feee76b037cb9a94183
reeli <reeli.null@gmail.com>
2021-11-07 14:07:28 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,51 @@
 import { NodePath } from "@babel/traverse";
 import {
-  ArrowFunctionExpression,
-  CallExpression,
-  Comment,
-  CommentBlock,
-  FunctionExpression,
-  Identifier,
-  VariableDeclarator,
+  JSXAttribute,
+  jsxExpressionContainer,
+  arrowFunctionExpression,
+  jsxAttribute,
+  jsxIdentifier,
+  isObjectExpression,
+  Expression,
+  identifier,
+  tsTypeAnnotation,
+  tSTypeReference,
+  parenthesizedExpression,
 } from "@babel/types";
+import { Visitor } from "@babel/core";
 
-const PURE_ANNOTATION = "#__PURE__";
-
-const isPureAnnotated = (comments: ReadonlyArray<Comment> | null): boolean => {
-  if (comments && comments.length > 0) {
-    return comments[comments.length - 1].value === PURE_ANNOTATION;
-  }
-  return false;
+const createParamWithType = (name: string, type: string) => {
+  const id = identifier(name);
+  id.typeAnnotation = tsTypeAnnotation(tSTypeReference(identifier(type)));
+  return id;
 };
 
-const containsNodePath = (nodePaths: NodePath[], nodePath: NodePath) => {
-  return nodePaths.indexOf(nodePath) > -1;
-};
+const createJsxAttribute = (name: string, value: Expression) => {
+  const id = createParamWithType("theme", "Theme");
 
-const createComponentBlock = (value: string): Comment =>
-  ({
-    type: "CommentBlock",
-    value,
-  } as CommentBlock);
-
-const isDeclaredVariableUsedInCallback = (
-  nodePath: NodePath<CallExpression>,
-) => {
-  let used = false;
-
-  if (nodePath.parentPath.isVariableDeclarator()) {
-    const scope = nodePath.parentPath.scope;
-    const id = (nodePath.parentPath as NodePath<VariableDeclarator>).get("id");
-
-    const traverseReferencedIdentifier = (np: NodePath<any>) =>
-      np.traverse({
-        ReferencedIdentifier(n: NodePath<Identifier>) {
-          if (scope.hasOwnBinding(n.node.name)) {
-            if (id.isIdentifier()) {
-              if (id.node.name === n.node.name) {
-                used = true;
-              }
-            }
-          }
-        },
-      } as any);
-
-    nodePath.traverse({
-      ArrowFunctionExpression: (
-        nodePath: NodePath<ArrowFunctionExpression>,
-      ) => {
-        traverseReferencedIdentifier(nodePath);
-      },
-      FunctionExpression: (nodePath: NodePath<FunctionExpression>) => {
-        traverseReferencedIdentifier(nodePath);
-      },
-    });
-  }
-
-  return used;
-};
-
-const isPureCall = (nodePath: NodePath<CallExpression>) => {
-  // x = pureCall()
-  // a.x = pureCall()
-  if (nodePath.parentPath.isAssignmentExpression()) {
-    return true;
-  }
-
-  // [pureCall()]
-  if (nodePath.parentPath.isArrayExpression()) {
-    return true;
-  }
-
-  // ({ x: prueCall(0,1) })
-  if (nodePath.parentPath.isProperty()) {
-    return true;
-  }
-
-  // const x = pureCall()
-  if (nodePath.parentPath.isVariableDeclarator()) {
-    return !isDeclaredVariableUsedInCallback(nodePath);
-  }
-
-  // otherCall(pureCall())
-  if (nodePath.parentPath.isCallExpression()) {
-    return containsNodePath(
-      nodePath.parentPath.get("arguments") || ([] as NodePath[]),
-      nodePath,
-    );
-  }
-
-  return false;
+  return jsxAttribute(
+    jsxIdentifier(name),
+    jsxExpressionContainer(
+      arrowFunctionExpression([id], parenthesizedExpression(value!)),
+    ),
+  );
 };
 
 export default () => ({
-  name: "pure-calls-annotation",
-  inherits: require("@babel/plugin-syntax-dynamic-import").default,
+  name: "emotion-css-transform",
   visitor: {
-    CallExpression: {
-      enter(nodePath: NodePath<CallExpression>) {
-        if (isPureCall(nodePath)) {
-          if (!isPureAnnotated(nodePath.node.leadingComments)) {
-            const pureAnnotation = createComponentBlock(PURE_ANNOTATION);
-
-            nodePath.replaceWith({
-              ...nodePath.node,
-              leadingComments: nodePath.node.leadingComments
-                ? nodePath.node.leadingComments.concat(pureAnnotation)
-                : [pureAnnotation],
-            } as any);
-          }
+    JSXAttribute: {
+      enter(nodePath: NodePath<JSXAttribute>) {
+        const valueExpression = (nodePath.node.value as any).expression;
+        if (
+          nodePath.node.name.name === "css" &&
+          isObjectExpression(valueExpression)
+        ) {
+          nodePath.replaceWith(
+            createJsxAttribute(nodePath.node.name.name, valueExpression),
+          );
         }
       },
     },
-  },
+  } as Visitor,
 });
